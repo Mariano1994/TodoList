6a2d43b1447adb7bc40b9ceceffadbd0d6b6ac9f
Mariano <marianocapiliku@gmail.com>
2023-09-11 12:47:09 +0100

    Create NewTaks input components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-
-import './App.css'
+import "./App.css";
 
 // Imagans
-import todoLogo from "./assets/todo.svg"
-import rocketLogo from "./assets/rocket.svg"
+import todoLogo from "./assets/todo.svg";
+import rocketLogo from "./assets/rocket.svg";
 
 function App() {
   return (
     <>
-      <header className='header'>
-        <div className='logo'>
+      <header className="header">
+        <div className="logo">
           <img src={rocketLogo} alt="rocketIlustrationLogo" />
           <img src={todoLogo} alt="" />
         </div>
       </header>
+      
+        <div className="input-task">
+          <form>
+            <input type="text" placeholder="Adicione nova tarefa" />
+            <button>Criar</button>
+          </form>
+        </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
