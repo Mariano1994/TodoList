f08ea00a7efa2a10f174765b5dad4de6000df6c3
Mariano <marianocapiliku@gmail.com>
2023-09-12 11:35:56 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { Header } from "./components/Header/Header";
 import { NewTask } from "./components/NewTask/NewTask";
 import { Summary } from "./components/Summary/Summary";
